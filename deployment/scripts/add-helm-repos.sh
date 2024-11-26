67b174f4318164ac58d132cc60c08fc8d379a7b5
w-jfe <w-jfe@werum.de>
2024-11-26 09:33:46 +0100

    add route templates, new helm chart and identity api

--- deployment/scripts/add-helm-repos.sh
+++ deployment/scripts/add-helm-repos.sh
@@ -3,3 +3,4 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add permitio https://permitio.github.io/opal-helm-chart
 helm repo add apisix https://charts.apiseven.com
+helm repo add eoepca https://eoepca.github.io/helm-charts
---
+++ deployment/scripts/uninstall-identity-api.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+helm uninstall -n iam identity-service
