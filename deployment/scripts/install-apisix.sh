9be77785a895766f5d9e17ed10a231471b0635ac
w-scho <w-scho@werum.de>
2026-05-21 09:59:39 +0200

    Adapt example scripts
    - Removed Identity API scripts
    - Updated chart version in install-apisix.sh

--- deployment/scripts/install-apisix.sh
+++ deployment/scripts/install-apisix.sh
@@ -3,7 +3,4 @@
 VALUES_YAML_DIR=${VALUES_YAML_DIR:-.}
 APISIX_VALUES_YAML=${APISIX_VALUES_YAML:-${VALUES_YAML_DIR}/apisix-values-asf.yaml}
 
-helm install --create-namespace -n iam apisix apisix/apisix --version 2.9.0 -f $APISIX_VALUES_YAML
-
-# Bitnami (older chart version):
-#helm install apisix oci://registry-1.docker.io/bitnamicharts/apisix --version 2.5.8 -n iam -f $APISIX_VALUES_YAML
+helm install --create-namespace -n iam apisix apisix/apisix --version 2.13.0 -f $APISIX_VALUES_YAML
